4f2a1fae601597f62d290d4f71993ed7b81ac003
Jeici <jcpablo10@gmail.com>
2021-05-23 15:02:46 -0500

    Setting sass files and assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('test webpack')
+import './styles/index.scss';
+import imgHeaderLogo from './images/logo_lavasconia.png'
+
+const headerLogo = document.querySelector('#header__logo');
+
+headerLogo.src = imgHeaderLogo
