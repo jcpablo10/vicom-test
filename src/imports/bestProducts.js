46ffdb3871539dac7af884017960f046a9c0cffc
Jeici <jcpablo10@gmail.com>
2021-05-23 19:13:39 -0500

    slider best sell products

---
+++ src/imports/bestProducts.js
@@ -0,0 +1,38 @@
+import best1 from '../images/best1.webp'
+import best2 from '../images/best2.webp'
+import best3 from '../images/best3.webp'
+import best4 from '../images/best4.webp'
+import best5 from '../images/best5.png'
+import best6 from '../images/best6.webp'
+import best7 from '../images/best7.webp'
+import best8 from '../images/best8.png'
+import best9 from '../images/best1.webp'
+import best10 from '../images/best2.webp'
+import best11 from '../images/best3.webp'
+import best12 from '../images/best4.webp'
+import best13 from '../images/best5.png'
+import best14 from '../images/best6.webp'
+import best15 from '../images/best7.webp'
+import best16 from '../images/best8.png'
+
+const images = [
+    best1,
+    best2,
+    best3,
+    best4,
+    best5,
+    best6,
+    best7,
+    best8,
+    best9,
+    best10,
+    best11,
+    best12,
+    best13,
+    best14,
+    best15,
+    best16,
+]
+
+
+export default images 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import imgIconCart from './images/icon_cart.svg'
 import imgEkco from './images/EKCO.png'
 import imgHeroBanner from './images/hero_banner.png'
 import imgRecipe from './images/receta_1.png'
+import imgSliderList from './imports/bestProducts.js'
 
 const headerLogo = document.querySelector('#header__logo');
 const iconUser = document.querySelector('#icon_user');
@@ -12,6 +13,8 @@ const iconCart = document.querySelector('#icon_cart');
 const logoEkco = document.querySelector('#logo_ekco');
 const heroBanner = document.querySelector('#hero_banner')
 const recipeImg = document.querySelector('#recipe_img')
+const sliderImgList = document.querySelectorAll('.slider__img')
+const sliderImgAsArray = [...sliderImgList]
 
 headerLogo.src = imgHeaderLogo
 iconUser.src = imgIconUser
@@ -19,3 +22,9 @@ iconCart.src = imgIconCart
 logoEkco.src = imgEkco
 heroBanner.src = imgHeroBanner
 recipeImg.src = imgRecipe
+
+console.log(sliderImgAsArray)
+
+sliderImgAsArray.map((item,idx) => {
+    item.src = imgSliderList[idx]
+})
